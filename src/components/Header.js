4b8e5bde61cce6a3de0ac14eb91d7366d370b348
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2021-09-02 14:52:17 -0600

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -23,7 +23,7 @@ const Header = props => (
         <li>
           <button
             onClick={() => {
-              props.onOpenArticle('about me')
+              props.onOpenArticle('intro')
             }}
           >
             Intro
@@ -32,7 +32,7 @@ const Header = props => (
         <li>
           <button
             onClick={() => {
-              props.onOpenArticle('work experience')
+              props.onOpenArticle('work')
             }}
           >
             Work
@@ -41,7 +41,7 @@ const Header = props => (
         <li>
           <button
             onClick={() => {
-              props.onOpenArticle('education')
+              props.onOpenArticle('about')
             }}
           >
             About
@@ -50,7 +50,7 @@ const Header = props => (
         <li>
           <button
             onClick={() => {
-              props.onOpenArticle('contact me')
+              props.onOpenArticle('contact')
             }}
           >
             Contact
