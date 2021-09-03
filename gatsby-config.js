cfce7090af304c469844abf8cc3e0017d155fcda
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2021-09-03 11:05:38 -0600

    Update gatsby-config.js

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Starter - Dimension V2',
-    author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    title: 'Jacob M. Roberts',
+    author: 'Jacob M. Roberts',
+    description: 'A site to host CV and Resume information',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
