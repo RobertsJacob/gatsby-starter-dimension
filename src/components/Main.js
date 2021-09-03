e5a80f653e14ecc5757dac2560a2f523f5b26fd3
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2021-09-03 13:23:30 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -3,8 +3,7 @@ import React from 'react'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
-
-class Main extends React.Component 
+ 
 class Main extends React.Component {
   constructor(props) {
     super(props);
