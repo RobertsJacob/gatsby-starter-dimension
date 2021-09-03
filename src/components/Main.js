4e7ef0821ac35d534c6b4f683a609af3e04908f1
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2021-09-03 12:02:26 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -121,7 +121,7 @@ class Main extends React.Component {
           style={{ display: 'none' }}
         >
           <h2 className="major">Contact Me</h2>
-         <form onSubmit={sendEmail}>
+         <form onSubmit={this.sendEmail}>
             <div className="field half first">
               <label htmlFor="name">Name</label>
               <input type="text" name="name" id="name" />
