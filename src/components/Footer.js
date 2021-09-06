30220b5f90e443eb57ba1e170729d260afeeede1
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2021-09-06 13:20:39 -0600

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types'
 
 const Footer = (props) => (
     <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
-        <p className="copyright">&copy; Jacob M Roberts with <a href="https://kylemckell.com/ ">Kyle McKell</a>. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
+        <p className="copyright">; Jacob M Roberts with <a href="https://kylemckell.com/ ">Kyle McKell</a>. Design: <a href="https://html5up.net">HTML5 UP</a>. Built with: <a href="https://www.gatsbyjs.org/">Gatsby.js</a></p>
     </footer>
 )
 
