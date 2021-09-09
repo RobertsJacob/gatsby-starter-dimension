8344b31095633f40971915e343a92877e8cfe184
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2021-09-09 11:26:42 -0600

    Update Main.js

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,5 @@
 import PropTypes from 'prop-types'
 import React from 'react'
-import emailjs from 'emailjs-com'
-import './ContactUs.css'
 import pic01 from '../images/pic01.jpg'
 import pic02 from '../images/pic02.jpg'
 import pic03 from '../images/pic03.jpg'
