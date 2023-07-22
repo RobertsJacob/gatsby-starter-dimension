c48affdff100cd1ef426e9a25f6b5d569192be99
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2023-07-21 20:59:06 -0600

    Update cards.js

--- src/pages/cards.js
+++ src/pages/cards.js
@@ -11,9 +11,8 @@ const SecondPage = () => (
     <div>
     <img style={{ width: 360, height: 432 }} src={card} alt="" />
     </div>
-    <p>Not finding what you are looking for?</p>
-    <Link to="/">Go to the homepage</Link>
-  <p className="copyright">Looking for other Jacob M Roberts Links? <a href="https://dot.cards/jacobrobertsloco/ ">Look Here on my Dot Profile!</a>.</p>
+    <p>Not finding what you are looking for? <a href="https://www.jacobmroberts.com/">Go to the Main Site</a></p>
+  <p className="copyright">Looking for other Jacob M Roberts links? <a href="https://dot.cards/jacobrobertsloco/ ">Take a look at my Dot Profile!</a>.</p>
   </Layout>
 )
 
