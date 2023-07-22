3f5492aa7308a4b5efcdc6fb290d8fbca86b371e
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2023-07-21 19:53:25 -0600

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -2,10 +2,14 @@ import React from 'react'
 import { Link } from 'gatsby'
 
 import Layout from '../components/layout'
+import education from '../images/education.jpg'
 
 const SecondPage = () => (
   <Layout>
-    <h1>Here lives my Business Cards</h1>
+    <h1>Here are my Business Cards</h1>
+    <h2>Edsports</h2>
+              <span className="image main">
+            <img src={education} alt="" />
     <p>Not finding what you are looking for?</p>
     <Link to="/">Go to the homepage</Link>
   </Layout>
