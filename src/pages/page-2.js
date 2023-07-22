1acf2d02b85822fd8f70abb926fba7a308b4fa92
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2023-07-21 20:42:49 -0600

    Update page-2.js

--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import PropTypes from 'prop-types'
+
 
 import Layout from '../components/layout'
 import card from '../images/card.png'
@@ -15,5 +17,15 @@ const SecondPage = () => (
     <Link to="/">Go to the homepage</Link>
   </Layout>
 )
+const Footer = (props) => (
+    <footer id="footer" style={props.timeout ? {display: 'none'} : {}}>
+        <p className="copyright">Looking for other Jacob M Roberts Links <a href="https://dot.cards/jacobrobertsloco/ ">Look Here!</a>.</p>
+    </footer>
+)
 
+Footer.propTypes = {
+    timeout: PropTypes.bool
+}
+
+export default Footer
 export default SecondPage
