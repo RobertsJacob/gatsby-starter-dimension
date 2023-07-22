fe62fa5da013e95554988976d86a546d39c8dead
RobertsJacob <88456699+RobertsJacob@users.noreply.github.com>
2023-07-21 21:03:06 -0600

    Update cards.js

--- src/pages/cards.js
+++ src/pages/cards.js
@@ -11,6 +11,14 @@ const SecondPage = () => (
     <div>
     <img style={{ width: 360, height: 432 }} src={card} alt="" />
     </div>
+    <h2>SVVSD</h2>
+    <div>
+    <img style={{ width: 360, height: 432 }} src={card} alt="" />
+    </div>
+    <h2>Windsor Charter Academy</h2>
+    <div>
+    <img style={{ width: 360, height: 432 }} src={card} alt="" />
+    </div>
     <p>Not finding what you are looking for? <a href="https://www.jacobmroberts.com/">Go to the Main Site</a></p>
   <p className="copyright">Looking for other Jacob M Roberts links? <a href="https://dot.cards/jacobrobertsloco/ ">Take a look at my Dot Profile!</a>.</p>
   </Layout>
